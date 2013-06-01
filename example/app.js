f3660876e35344e824562913e0184ecf9c276fcb
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-06-02 00:05:22 +0400

    sample

--- example/app.js
+++ example/app.js
@@ -3,21 +3,19 @@ var express = require('express'),
     path = require('path'),
     cachingMiddleware = require('./../');
 
-
 var app = express();
 
 app.configure(function () {
     app.set('port', process.env.PORT || 3000);
     app.use(express.logger('dev'));
-    app.use(cachingMiddleware(5000,{'type':'application/json'}));
 
-//other middlewares
-    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(cachingMiddleware(1000,{'type':'application/json'}));
+    app.use('/cacheFor5sec',cachingMiddleware(5000,{'type':'application/json'}));
+    app.use('/cacheFor3sec',cachingMiddleware(3000,{'type':'application/json'}));
+
     app.use(app.router);
-
     app.use(express.errorHandler());
 
-//static pages
     app.get('*', function(request,response){
         response.json({
             'Page Created At':new Date().toLocaleTimeString()
